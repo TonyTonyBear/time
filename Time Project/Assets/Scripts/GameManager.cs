b286fe5684de6a48a2e4096322f46d1d2b124ab8
Tony Barrett <ajbarrettx@gmail.com>
2020-05-18 19:13:40 -0400

    Add game state enums and OnGameStateUpdated event.

---
+++ Time Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { PLAYING, WON, LOST, MENU };
+
+public class GameManager : MonoBehaviour
+{
+    private GameState _currentGameState = GameState.PLAYING;
+    GameState currentGameState {get {return _currentGameState;}}
+
+    public delegate void GameStateAction(GameState newState);
+    public static event GameStateAction OnGameStateUpdated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateGameState(GameState newState)
+    {
+        if (newState == _currentGameState) return;
+
+        _currentGameState = newState;
+
+        // Call OnGameStateUpdated event.
+        OnGameStateUpdated(_currentGameState);
+    }
+
+    private void DisplayNewState(GameState newState)
+    {
+        string outputText = "The new state is: ";
+
+        switch (newState)
+        {
+            case (GameState.LOST):
+                outputText += "Lost";
+                break;
+            
+            case (GameState.MENU):
+                outputText += "Menu";
+                break;
+            
+            case (GameState.PLAYING):
+                outputText += "Playing";
+                break;
+            
+            case (GameState.WON):
+                outputText += "Won";
+                break;
+
+            default:
+                outputText += "Not Found";
+                break;
+        }
+
+        Debug.Log(outputText);
+    }
+}
