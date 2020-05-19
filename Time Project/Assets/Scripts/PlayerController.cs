d5ae81fc2568b07cdcfe065d7292fe06c86fd2ee
Tony Barrett <ajbarrettx@gmail.com>
2020-05-19 16:14:02 -0400

    Add Jumping and Falling functionality to the PlayerController.

--- Time Project/Assets/Scripts/PlayerController.cs
+++ Time Project/Assets/Scripts/PlayerController.cs
@@ -7,16 +7,31 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
     public float speed = 3f;
+    private Vector3 velocity;
+    private float gravityForce = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+    public float jumpHeight = 3f;
     
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        velocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -30,12 +45,19 @@ public class PlayerController : MonoBehaviour
         Vector3 right = cameraTransform.right;
         right.y = 0;
         right.Normalize();
-
-        Debug.DrawRay(transform.position, forward, Color.red, 100.0f);
         
         Vector3 moveVector = (right * x) + (forward * z);
 
         controller.Move(moveVector * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravityForce);
+        }
+
+        velocity.y += gravityForce * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
