e732c3f96610cdb9c8d8f35fb7368aafcd233671
Tony Barrett <ajbarrettx@gmail.com>
2020-05-18 21:38:17 -0400

    Fix movement logic to work with free look camera.

---
+++ Time Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    public float speed = 3f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Remove y fields from look vectors to prevent vertical movement.
+        Transform cameraTransform = Camera.main.transform;
+        Vector3 forward = cameraTransform.forward;
+        forward.y = 0;
+        forward.Normalize();
+
+        Vector3 right = cameraTransform.right;
+        right.y = 0;
+        right.Normalize();
+
+        Debug.DrawRay(transform.position, forward, Color.red, 100.0f);
+        
+        Vector3 moveVector = (right * x) + (forward * z);
+
+        controller.Move(moveVector * speed * Time.deltaTime);
+
+    }
+}
