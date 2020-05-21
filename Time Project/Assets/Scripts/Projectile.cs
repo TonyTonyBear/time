4f2fa8c031aba21cd0c32ffb47f763292e15bb2a
Tony Barrett <ajbarrettx@gmail.com>
2020-05-20 22:43:14 -0400

    Add projectile launcher to character.

--- Time Project/Assets/Scripts/Projectile.cs
+++ Time Project/Assets/Scripts/Projectile.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed = 1f;
+    [SerializeField] private float lifetime = 10.0f;
+    private float currentTimer = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
+
+        currentTimer += Time.deltaTime;
+
+        if (currentTimer >= lifetime)
+        {
+            Debug.LogWarning("Projectile lifetime reached: " + this.GetInstanceID());
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter(Collider otherCollider)
     {
+        if (otherCollider.tag == "Slowdown") return;
+
         Debug.Log("Destroying projectile: " + this.GetInstanceID());
 
         Destroy(gameObject);
---
+++ Time Project/Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public Projectile projectile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Launching Projectile.");
+            LaunchProjectile();
+        }
+    }
+
+    private void LaunchProjectile()
+    {
+        // Create projectile at spawnPoint.
+        GameObject.Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
+    }
+}
