c17304eaf52fbe7e7661890c9a6a5cf6f9618f38
Tony Barrett <ajbarrettx@gmail.com>
2020-05-20 22:30:48 -0400

    Add Projectile prefab with collision.

---
+++ Time Project/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
+    }
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        Debug.Log("Destroying projectile: " + this.GetInstanceID());
+
+        Destroy(gameObject);
+    }
+}
